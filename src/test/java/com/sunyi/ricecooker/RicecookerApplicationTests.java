dbc83dcd31c5a29bdd46e1613ebb7d02f64d68a5
MrY <SuperOranges@users.noreply.github.com>
2020-03-15 23:55:38 +0800

    新增爬虫模块的研究

---
+++ src/test/java/com/sunyi/ricecooker/RicecookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyi.ricecooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicecookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
